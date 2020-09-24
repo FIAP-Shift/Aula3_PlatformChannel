9eb072bbf6373227ca64eb32a76269487bbb2913
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-09-23 21:37:28 -0300

    classe de constantes

---
+++ lib/util/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color colorBackBlue1 = Color.fromARGB(255, 65, 105, 216);
+Color colorBackBlue2 = Color.fromARGB(255, 58, 95, 200);
+Color colorBackBlue3 = Color.fromARGB(255, 36, 70, 166);
+Color colorBlueText = Color.fromARGB(255, 39, 72, 167);
