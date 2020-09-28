0fdb2d40faa783c2d1ff10772d6263dab9bc37d7
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-09-28 20:47:28 -0300

    uso do platform channel

--- android/app/src/main/kotlin/ogliari/flutter/flutter_shift_platform_channel/MainActivity.kt
+++ android/app/src/main/kotlin/ogliari/flutter/flutter_shift_platform_channel/MainActivity.kt
@@ -1,6 +1,109 @@
 package ogliari.flutter.flutter_shift_platform_channel
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.annotation.NonNull
+import com.pubnub.api.PNConfiguration
+import com.pubnub.api.PubNub
+import com.pubnub.api.callbacks.SubscribeCallback
+import com.pubnub.api.models.consumer.PNStatus
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult
+import com.pubnub.api.models.consumer.pubsub.PNSignalResult
+import com.pubnub.api.models.consumer.pubsub.message_actions.PNMessageActionResult
+import com.pubnub.api.models.consumer.pubsub.objects.PNMembershipResult
+import com.pubnub.api.models.consumer.pubsub.objects.PNSpaceResult
+import com.pubnub.api.models.consumer.pubsub.objects.PNUserResult
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import java.util.Arrays
 
 class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "game/exchange"
+    private var pubnub: PubNub? = null
+    private var channel: String? = null
+    private var handler: Handler? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        handler = Handler(Looper.getMainLooper())
+
+        //criem suas chaves , porque essas serão removidas daqui 1 ou 2 semanas
+        //fácil fácil no página do pubnub
+        val pnConfiguration = PNConfiguration()
+        pnConfiguration.subscribeKey = "sub-c-b46b44f4-b357-11ea-a40b-6ab2c237bf6e"
+        pnConfiguration.publishKey = "pub-c-e64c54b3-4076-4638-a0d6-f17d9a5e5442"
+        pnConfiguration.uuid = "myUniqueUUID"
+        pubnub = PubNub(pnConfiguration)
+
+        //PUBNUB é o canal na nuvem, ou seja, ele é o middleware entre os agentes do jogo
+        pubnub?.let {
+            it.addListener(object : SubscribeCallback() {
+                override fun status(pubnub: PubNub, status: PNStatus) { }
+
+                override fun message(pubnub: PubNub, message: PNMessageResult) {
+                    //aqui tratamos a mensagem recebida do pubnub e precisamos enviar
+                    //ao código DART
+                    val receivedMessageObject = message.message.asJsonObject["tap"]
+                    Log.e("pubnub", "Received message content: $receivedMessageObject")
+
+                    handler?.let {
+                        it.post {
+                            //chgamada do nativo pro dart
+                            MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL)
+                                .invokeMethod(
+                                    "sendAction",
+                                    "${receivedMessageObject.asString}"
+                                );
+                        }
+                    }
+                }
+
+                override fun presence(pubnub: PubNub, presence: PNPresenceEventResult) {}
+                override fun signal(pubnub: PubNub, pnSignalResult: PNSignalResult) {}
+                override fun user(pubnub: PubNub, pnUserResult: PNUserResult) {}
+                override fun messageAction(pubnub: PubNub, pnMessageActionResult: PNMessageActionResult) {}
+                override fun membership(pubnub: PubNub, pnMembershipResult: PNMembershipResult) {}
+                override fun space(pubnub: PubNub, pnSpaceResult: PNSpaceResult) {}
+            })
+        }
+
+    }
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        //METHODCHANNEL é o canal entre o nativo e o dart (não confundam com o canal do Pubnub)
+        //com o setMethodCallHandler, nós ouvimos requisições do DART a serem tratadas pelo lado nativo
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            //tratamento dos métodos requisitados pelo DART
+            if (call.method == "sendAction") {
+                pubnub!!.publish()
+                    .message(call.arguments)
+                    .channel(channel)
+                    .async { result, status ->
+                        Log.e( "pubnub","teve erro? ${status.isError}")
+                    }
+                result.success(true)
+            } else if (call.method == "subscribe") {
+                subscribeChannel(call.argument("channel"))
+                result.success(true)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    fun subscribeChannel(channelName: String?){
+        channel = channelName
+        channelName?.let {
+            pubnub?.subscribe()?.channels(Arrays.asList(channelName))?.execute();
+        }
+    }
+
 }
--- lib/screen/game.dart
+++ lib/screen/game.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_shift_platform_channel/model/message.dart';
 import 'package:flutter_shift_platform_channel/util/constants.dart';
 
 //essa classe controla o início do jogo
@@ -33,6 +35,42 @@ class _GameState extends State<Game> {
 
   bool minhaVez;
   WrapperCreator creator;
+
+  // 0 = branco. 1 = eu. 2 - adversário
+  List<List<int>> cells = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ];
+
+  static const platform = const MethodChannel('game/exchange');
+
+  @override
+  void initState() {
+    super.initState();
+    configureCallHandler();
+  }
+
+  void configureCallHandler(){
+    //tratamos chamadas do código nativo para o dart
+    platform.setMethodCallHandler((call) {
+        String argumentos = call.arguments.toString();
+        List<String> parts = argumentos.split("|");
+        ExchangeMessage message = ExchangeMessage(parts[0], int.parse(parts[1]), int.parse(parts[2]));
+
+        //só ouço mensagens do meu oponente
+        if (message.user == (creator.creator ? 'p2' : 'p1')) {
+          setState(() {
+            minhaVez = true;
+            cells[message.x][message.y] = 2;
+          });
+          //checkWinner();
+        }
+
+        return;
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,6 +107,24 @@ class _GameState extends State<Game> {
           ) : Text(
               minhaVez ? "Sua Vez!!" : "Aguarde Sua Vez!!!",
               style: textStyle36
+          ),
+          GridView.count(
+            shrinkWrap: true,
+            padding: EdgeInsets.all(20),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 3,
+            children: <Widget>[
+              getCell(0, 0),
+              getCell(0, 1),
+              getCell(0, 2),
+              getCell(1, 0),
+              getCell(1, 1),
+              getCell(1, 2),
+              getCell(2, 0),
+              getCell(2, 1),
+              getCell(2, 2),
+            ],
           ),
         ],
       ),
@@ -109,7 +165,101 @@ class _GameState extends State<Game> {
             style: textStyle36
         ),
       ),
+      onPressed: (){
+        createGame(owner);
+      },
     ),
   );
+
+  Future<void> createGame(bool isCreator) {
+    TextEditingController editingController = TextEditingController();
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Qual o nome do jogo?'),
+          content: TextField(
+            controller: editingController,
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Jogar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _sendAction(
+                    'subscribe',//método desejado
+                    {'channel': editingController.text}//argumento
+                    ).then((value) { //ou usa then ou usa async-await
+                  setState(() {
+                    creator = WrapperCreator(isCreator, editingController.text);
+                    minhaVez = isCreator;
+                  });
+                });
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget getCell(int x, int y) => InkWell(
+        child: Container(
+          padding: EdgeInsets.all(8),
+          child: Center(
+              child: Text(
+                  cells[x][y] == 0 ? " " : cells[x][y] == 1 ? "X" : "O",
+                  style: textStyle75
+              )
+          ),
+          color: Colors.lightBlueAccent,
+        ),
+        onTap: () {
+          if (minhaVez && cells[x][y] == 0) {
+            _showSendingAcion();
+            _sendAction('sendAction', {'tap': '${creator.creator ? 'p1' : 'p2'}|$x|$y'})
+                .then((value) {
+                Navigator.of(context).pop();
+                minhaVez = false;
+                cells[x][y] = 1;
+
+                setState(() {});
+              //checkWinner();
+            });
+          }
+        },
+      );
+
+  Future<bool> _sendAction(String action, Map<String, dynamic> arguments) async {
+    try {
+      //na linha de baixo estamos fazendo, efetivamente, a chamada a um método nativo
+      final bool result = await platform.invokeMethod(action, arguments);
+      if (result){
+        return true;
+      }
+    } on PlatformException catch (e) {
+      return false;
+    }
+  }
+
+  Future<void> _showSendingAcion() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Enviando ação, aguarde...'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircularProgressIndicator()
+            ],
+          ),
+        );
+      },
+    );
+  }
 
 }
